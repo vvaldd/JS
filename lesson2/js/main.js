b6343e8a8f1021be4d39ef26d4ab1f2ee9948769
vvaldd <wroztorguev@gmail.com>
2021-02-10 22:25:39 +0200

    1

---
+++ lesson2/js/main.js
@@ -0,0 +1,77 @@
+/*-створити масив та вивести його в консоль:
+- з 5 числових значень
+- з 5 стічкових значень
+- з 5 значень стрічкового, числового та булевого типу*/
+/*let number = [10, 20, 30, 40, 50];
+let string = ['sky', 'mew', 'go', 'armed', 'so'];
+let combo = ['lao', 45, 12, true, 'false'];
+    console.log(number, string, combo);*/
+
+/*Створити пустий масив.
+Наповнити його будь-якими значеннями звертаючись до конкретного індексу.
+Вивести в консоль*/
+/*let clear = [];
+clear[0] = 'str';
+clear[1] = 12;
+clear[2] = true;
+clear.push(999);
+    console.log(clear);*/
+
+    /*Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.*/
+    /*let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = 0; i < 10; i++) {
+        console.log(numbers[i]);
+    }*/
+
+    /*Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.*/
+    /*let words = ['idea', 'identity', 'ill', 'illegal', 'imagine', 'important', 'intend', 'interesting', 'irritable', 'item'];
+        for (let word of words) {
+            console.log(word);
+        }*/
+    
+    /*Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.*/
+    /*let combo = ['idea', false, 234, true, 'identity', 'imagine', 426, 345, true, 12];
+        for (let i = 0; i < combo.length; i++) {
+            console.log(combo[i]);
+        }*/
+    
+    /* - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+    - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+    - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи*/
+    /*let combo = ['idea', false, 234, true, 'identity', 'imagine', 426, 345, true, 12];   
+    for (let i = 0; i < 10; i++) {
+        if (typeof combo[i] === "boolean") {
+            console.log(combo[i]);
+        }
+    }
+    for (let i of combo) {
+        if (typeof i === 'number'){
+            console.log(i);
+        }
+    }
+    
+    for (let i of combo) {
+        if (typeof i === 'string'){
+            console.log(i);
+        }
+    }*/
+
+
+
+
+
+    
+    /*- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+    - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині*/
+    /*for (let i = 0; i < 10; i++) {
+        document.write('<div>test</div>');
+        document.write('<hr>');
+    }*/
+    /*for (let i = 0; i < 10; i++) {
+        document.write('<div>test ${i}</div>');
+        document.write('<hr>');
+    }*/
+
+
+
+
